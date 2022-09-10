6adb7f786290a1726e4ce41d85084ee25e7aba5a
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-09-09 20:08:50 -0400

    update constant base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = 'token';
-export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
+export const BASE_URL = "https://positive-theme-357914.uk.r.appspot.com";
 export const SEARCH_KEY = {
     all: 0,
     keyword: 1,
